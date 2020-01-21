07670d28b3d43e47cf639fbed8b0a054a74b8281
Roman Soldatov <s.roma245@gmail.com>
2020-01-21 19:34:37 +0300

    Wrote Selection and Intersection sorting

--- src/week1/Solution.java
+++ src/week1/Solution.java
@@ -1,4 +1,56 @@
 package week1;
 
+import java.util.Scanner;
+
 public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+
+        insertionSort(a);
+
+        // output
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+
+    }
+
+    // Selection sort
+    public static void selectionSort(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            int min = a[i];
+            int index = i;
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[j] < min) {
+                    min = a[j];
+                    index = j;
+                }
+            }
+
+            int temp = a[i];
+            a[i] = min;
+            a[index] = temp;
+        }
+    }
+
+    // Insertion sort
+    public static void insertionSort(int[] a) {
+        for (int i = 1; i < a.length; i++) {
+            int key = a[i];
+            int j = i - 1;
+
+            while (j >= 0 && a[j] > key) {
+                a[j + 1] = a[j];
+                j--;
+            }
+            a[j + 1] = key;
+        }
+    }
 }
+

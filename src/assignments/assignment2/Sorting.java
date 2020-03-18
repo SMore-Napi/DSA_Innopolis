11d0815318878f89d60089025dbc76abd3654c1b
Roman Soldatov <s.roma245@gmail.com>
2020-03-18 11:53:10 +0300

    Assignment 2: 2.2 Sorting. Primitive merge sort, array generator

--- src/assignments/assignment2/Sorting.java
+++ src/assignments/assignment2/Sorting.java
@@ -1,5 +1,7 @@
 package assignments.assignment2;
 
+import java.util.Arrays;
+
 /**
  * @author Roman Soldatov BS19-02.
  * 2.2 Sorting.
@@ -11,6 +13,67 @@ package assignments.assignment2;
  */
 public class Sorting {
     public static void main(String[] args) {
+        int [] array = getRandomArray(100, 15);
+        System.out.println(Arrays.toString(array));
+        MergeSort.mergeSort(array);
+        System.out.println(Arrays.toString(array));
 
     }
+
+    public static int[] getRandomArray(int maxLength, int maxValue) {
+
+        int length = (int) (Math.random() * maxLength);
+        int[] array = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            array[i] = (int) (Math.random() * maxValue);
+        }
+
+        return array;
+    }
 }
+
+class MergeSort {
+    private static int[] helper;
+
+    public static void mergeSort(int[] arr) {
+        helper = new int[arr.length];
+        sort(arr, 0, arr.length - 1);
+    }
+
+    private static void sort(int[] arr, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int mid = low + (high - low) / 2; // граница разбиения
+
+        sort(arr, low, mid); // сортируем левую часть
+        sort(arr, mid + 1, high); // сортируем правую часть
+
+        merge(arr, low, mid, high);
+    }
+
+    private static void merge(int[] arr, int low, int mid, int high) {
+        int i = low;
+        int j = mid + 1;
+        for (int k = low; k <= high; k++) {
+            helper[k] = arr[k];
+        }
+        int k = low;
+        while (i <= mid && j <= high) {
+            if (helper[i] < helper[j]) {
+                arr[k++] = helper[i++];
+            } else {
+                arr[k++] = helper[j++];
+            }
+        }
+        // выписываем подряд из левой части, если остались
+        while (i <= mid) {
+            arr[k++] = helper[i++];
+        }
+        while (j <= high) {
+            arr[k++] = helper[j++];
+        }
+    }
+}
+

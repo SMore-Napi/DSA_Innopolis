83676bfa0328e0c59ecd42d2c99ad15a6a44337c
smore <s.roma245@gmail.com>
2020-04-19 21:24:58 +0400

    Week 13: Kruskal's algorithm. Some changes.

--- src/week13/Main.java
+++ src/week13/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 
 public class Main {
+
     public static void main(String[] args) {
         Graph<String, Integer> roads = new Graph<>();
         roads.addEdge("Kazan", "Innopolis", 50);
@@ -46,15 +47,15 @@ class DisjointIntSets {
         this.parents[this.find(i)] = this.find(j);
     }
 
-    // {0, 1, 2, 3, 4, 5}
-    // {{0 <- 2}, {1 -> 5 -> 4}, {3}}
+    // { 0, 1, 2, 3, 4, 5 }
+    // { {0 <- 2}, {1 -> 5 -> 4}, {3} }
 
-    // { {   2 -> 0
-    //            |
-    //            v
-    //  1 -> 5 -> 4}
+    // { {    2 -> 0
+    //             |
+    //             v
+    //   1 -> 5 -> 4}
 
-    // {1, 2, 1, 3, 2, 2}
+    // { 1, 2, 1, 3, 2, 2 }
 
 
 }
@@ -79,7 +80,10 @@ class Graph<V, E extends Comparable> {
 
         @Override
         public String toString() {
-            return vertices.get(this.from).toString() + " -(" + this.weight.toString() + ")- " + vertices.get(this.to).toString();
+            return
+                    vertices.get(this.from).toString()
+                            + " -(" + this.weight.toString() + ")- "
+                            + vertices.get(this.to).toString();
         }
     }
 
@@ -94,7 +98,7 @@ class Graph<V, E extends Comparable> {
         this.edges = new ArrayList<>();
     }
 
-    int addVertexNotExists(V vertex) {
+    public int addVertexIfNotExists(V vertex) {
         for (int i = 0; i < this.vertices.size(); i++) {
             if (this.vertices.get(i).equals(vertex)) {
                 return i;
@@ -104,10 +108,10 @@ class Graph<V, E extends Comparable> {
         return (this.vertices.size() - 1);
     }
 
-    void addEdge(V from, V to, E weight) {
-        int fromIndex = this.addVertexNotExists(from);
-        int toIndex = this.addVertexNotExists(to);
-        this.edges.add(new Edge(fromIndex, toIndex, weight));
+    public void addEdge(V from, V to, E weight) {
+        int fromIndex = this.addVertexIfNotExists(from);
+        int toIndex = this.addVertexIfNotExists(to);
+        this.addEdge(new Edge(fromIndex, toIndex, weight));
     }
 
     public void addEdge(Edge edge) {
@@ -115,7 +119,7 @@ class Graph<V, E extends Comparable> {
     }
 
     public Graph<V, E> kruskalMST() {
-        Graph<V, E> mst = new Graph<>(); // O(V)
+        Graph<V, E> mst = new Graph<>(this.vertices);  // O(V)
 
         DisjointIntSets forest = new DisjointIntSets(this.vertices.size()); // O(V)
 
@@ -127,7 +131,7 @@ class Graph<V, E extends Comparable> {
         for (Edge edge : this.edges) {
             if (forest.find(edge.from) != forest.find(edge.to)) {
                 forest.union(edge.from, edge.to);
-                mst.addEdge(this.vertices.get(edge.from), this.vertices.get(edge.to), edge.weight);
+                mst.addEdge(edge);
             }
         }
 
